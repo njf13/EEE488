342baf76348935a637f4a9252c891cb593ec999c
jwrussert <jwrussert@gmail.com>
2019-04-01 14:03:13 -0700

    Overlay plots (#15)
    * Just committed the Test_lsys.m file so it would be current
    
    * just started some changes. not finished
    
    * okay try again. Just some unfinished changes
    
    * Created an optional overlay vector to the plot function to allow user to plot heat maps of dendrite nodes

--- Trees_scripts/Test_lsys.m
+++ Trees_scripts/Test_lsys.m
@@ -1,14 +1,14 @@
 clear
 
-a = ["A", "[+F[+F[+F[+F]-FB]-F[+F]-F]-F]-F[+F[+F[+F]-FA]-F[+F]-F]-FB", 1];
+a = ["A", "[+F[+F[+F[+F]-FB]-F[+F]-F]-F]-F[+F[+F[+F]-FA]-F]-FB", 1];
 b = ["B", "[+F[+F]-F]-F[+FB]-F", 1];
 
 %a = ["A", "F[-F]+FA", 1];
 
-l = lsystem("[FA]",[a;b], 15, false);
+l = lsystem("[FA]",[a;b], 17.5, false);
 %l.rules(1,:) = ["F", "F+F", 1];
 
-l.generate(5)
+l.generate(12)
 
 
 
