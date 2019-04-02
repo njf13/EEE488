6c2c128226744d621310f273b723ca69fa663e15
jwrussert <jwrussert@gmail.com>
2019-04-01 22:52:07 -0700

    Revised the netlist command to make it easier to use, and remove the bug where the caps at  the termination points were floating.

--- Trees_scripts/dendrite.m
+++ Trees_scripts/dendrite.m
@@ -169,23 +169,56 @@ classdef dendrite < handle
             
             disp(join(str,''));
             y = [y; str];
-            str = ['* Netlist created on ' , date(),"","","","","",""];
            
+            str = ['* Netlist created on ' , date(),"","","","",""];
             disp(join(str,''));
-            y = [y; str];
-            fprintf('\n')
+            y = [y; [str, "\n"]];
+           
+            str = [".param maxV = 10","","","","","", ""];
+            disp(join(str,''));
+            y = [y; [str, "\n"]];
+            
+            % Add analysis commands here
+            % All analysis commands except DC op will have an asterisk at
+            % the beginning to "comment" them out. That way the person
+            % doing the analysis can remove the asterisk for the desired
+            % analysis.
+            str = [".op", "","","","","",""];
+            disp(join(str,''));
+            y = [y;["\n", str]];
             
             % Place sources at all of the termination nodes.
-            str = ["Vsource ", "Vsource ", "gnd ", "PULSE (0 10 0 1n 1n 49n 100n)", "", "", "", ""];
+            str = ["Vsource ", "Vsource ", "gnd ", "{maxV} AC 1", "", "", "", ""];
             disp(join(str,''));
             y = [y;str];
+            
+            str = ["*.AC dec 10 10 1G", "","","","","","",""];
+            disp(join(str,''));
+            y = [y;str];
+            
+            str = ["*.tran 1ns 1", "","","","","","",""];
+            disp(join(str,''));
+            y = [y;str];
+            
+            % Create a source for the transisent analysis.
+            str = ["*Vsource ", "Vsource ", "gnd ", "PULSE (0 {maxV} 0 1n 1n 99m 200m)", "", "", "", ""];
+            disp(join(str,''));
+            y = [y;str];
+            
+            % Place sources at all of the termination nodes.
+            str = ["*.measure tran tdlay TRIG V(Vsource) VAL = 0.5*maxV RISE=3 TARG V(*) VAL = 0.5*maxV RISE=3", "", "", "", "","","",""];
+            disp(join(str,''));
+            y = [y;str];
+            
+            
+            
             
             % Place a 0V source from the beginning node to GND. Probably an
             % inefficent way to do this, but it simplifies the code for
             % now.
-            str = ["Vgnd ", "n1 ", "gnd ", "0", "", "" ,"", "" ];
+            str = ["Vgnd ", "n1 ", "gnd ", "0", "" ,"", "" ];
             disp(join(str,''));
-            y = [y;str];            
+            y = [y;[str, "\n\n"]];            
             
             for i = 1:obj.nodes
                 daughterNodes = find(obj.dA(:, i));
@@ -203,7 +236,7 @@ classdef dendrite < handle
                     disp(join(str,''));
                     y = [y;str];
                     
-                    str = ["c", branchCount, " n" , daughterNodes(j), " gnd ",c0*0.5^BO(i),"u",""];
+                    str = ["c", branchCount, node2, " gnd ",c0*0.5^BO(i),"u", "",""];
                     disp(join(str,''));
                     y = [y;str];
                     
@@ -211,22 +244,7 @@ classdef dendrite < handle
                 end
             end
             
-            % Add analysis commands here
-            % All analysis commands except DC op will have an asterisk at
-            % the beginning to "comment" them out. That way the person
-            % doing the analysis can remove the asterisk for the desired
-            % analysis.
-            str = [".op", "","","","","",""];
-            disp(join(str,''));
-            y = [y;["\n", str]];
             
-            str = ["*.tran 1ns 1u", "","","","","","",""];
-            disp(join(str,''));
-            y = [y;str];
-            
-            str = ["*.AC dec 10 10 1G", "","","","","","",""];
-            disp(join(str,''));
-            y = [y;str];
             
             % Add .end here
             str = [".end", "","","","","",""];
