243bfa053146e161d464775f25be1ba8d77a4a96
jwrussert <jwrussert@gmail.com>
2019-04-11 11:27:30 -0700

    got merge function working. Some modifications needed

---
+++ Trees_scripts/@dendrite/merge.m
@@ -0,0 +1,28 @@
+function newDendrite = merge(dend1, dend2, mergeNode)
+% A function to allow two dendrites to be combined into one.
+% The user will define "mergeNode" which is the node of obj1 (the first
+% dendrite) that will be combined with Node 1 of obj2 (the second dendrite)
+    dA = zeros(dend1.nodes + dend2.nodes - 1);
+    xOffset = dend1.X(mergeNode);
+    yOffset = dend1.Y(mergeNode);
+    zOffset = dend1.Z(mergeNode);
+    
+    newX = [dend1.X(1:mergeNode-1); dend2.X + xOffset; dend1.X(mergeNode+1:end)];
+    newY = [dend1.Y(1:mergeNode-1); dend2.Y + yOffset; dend1.Y(mergeNode+1:end)];
+    newZ = [dend1.Z(1:mergeNode-1); dend2.Z + zOffset; dend1.Z(mergeNode+1:end)];
+    
+    % Transfer over dA of dendrite 1. This has to be done in 4 parts.
+    % First transfer top left quadrant
+    dA(1:mergeNode,1:mergeNode) = dend1.dA(1:mergeNode,1:mergeNode);
+    % Then transfer top right quadrant
+    dA(1:mergeNode, mergeNode + dend2.nodes :end) = dend1.dA(1:mergeNode, mergeNode+1:end);
+    % Then transfer bottom left quadrant
+    dA(mergeNode + dend2.nodes :end,1:mergeNode) = dend1.dA(mergeNode+1:end,1:mergeNode);
+    % Then transfer the bottom right quadrant
+    dA(mergeNode + dend2.nodes :end, mergeNode + dend2.nodes:end)= dend1.dA(mergeNode+1:end,mergeNode+1:end)
+    
+    % Transfer over dA of dendrite 2
+    dA(mergeNode:mergeNode + dend2.nodes - 1, mergeNode:mergeNode + dend2.nodes - 1) = dend2.dA;
+    
+    newDendrite = dendrite(dA, newX, newY, newZ);
+end
