09b9c006f685898173eb294b2629d1ae9d9a8363
jwrussert <jwrussert@gmail.com>
2019-04-13 00:02:02 -0700

    Fixed DC analysis. Created length based R setting

--- Trees_scripts/@dendrite/dendrite.m
+++ Trees_scripts/@dendrite/dendrite.m
@@ -46,7 +46,7 @@ classdef dendrite < handle
         
         rValues = setR(obj,  calcMethod, inputVal) 
         capValues = setC(obj,  calcMethod, inputVal) 
-        y = DC( obj, Vdd)
+        [vNodes currents] = DC( obj, Vdd)
         ckt = makeCircuit(obj)
     end
 end
--- Trees_scripts/@dendrite/setR.m
+++ Trees_scripts/@dendrite/setR.m
@@ -32,6 +32,23 @@ function rValues = setR(obj,  calcMethod, inputVal)
                     rValues = inputVal*ones(obj.nodes,1);
                 case 'v' % Set R based on an input vector
                     rValues = inputVal;
+                case 'dist' % Calculate the R based on the distance between points
+                    rValues = zeros(obj.nodes,1);
+
+                    for ii = 2:obj.nodes
+                        % Find parent node
+                        parent = find(obj.dA(ii,:));
+                        % Find distance beween the points.
+                        deltaX = obj.X(ii)-obj.X(parent);
+                        deltaY = obj.Y(ii)-obj.Y(parent);
+                        distance = norm([deltaX deltaY]);
+                        
+                        if(distance == 0)
+                            rValues(ii) = inputVal/1000;
+                        else
+                            rValues(ii) = inputVal*norm([deltaX deltaY]);
+                        end
+                    end
                 otherwise
                     disp("Error!");
                     disp("The input must be one of the following:")
