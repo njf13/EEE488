b6ef67630cc57c3aef9c17a0036362d85574f5a4
jwrussert <jwrussert@gmail.com>
2019-04-16 13:03:24 -0700

    Changed ABCD and Y functions to work on tfs and doubles

--- Trees_scripts/Y2ABCD.m
+++ Trees_scripts/Y2ABCD.m
@@ -1,6 +1,15 @@
 function ABCD = Y2ABCD(Y)
-    s = tf('s');
-    ABCD = s*ones(2);
+% This function converts Y-parameters (admittance) to ABCD-parameters.
+% There is a similar function included in the MATLAB RF Toolbox, but it
+% only works on numbers (real or complex). This function works on both
+% numbers or transfer functions.
+    if(isa(Y,'tf'))
+        s = tf('s');
+        ABCD = s*ones(2);    
+    else
+        ABCD = zeros(2);
+    end
+
     ABCD(1,1) = -Y(2,2)/Y(2,1);
     ABCD(1,2) = -1/Y(2,1);
     ABCD(2,1) = -(Y(1,1)*Y(2,2)-Y(2,1)*Y(1,2))/Y(2,1);
